f4941f47ecbeeb2e18c09353ff62573db4644151
melodiessim98 <melodiessim98@gmail.com>
2019-05-18 23:37:36 +0800

    #120 Upstream PR

--- python/src/blockchain.py
+++ python/src/blockchain.py
@@ -81,16 +81,20 @@ class Blockchain:
 
         # Grab and verify the chains from all the nodes in our network
         for node in neighbours:
-            response = requests.get(f"http://{node}/chain")
+            try:
+                response = requests.get(f"http://{node}/chain")
 
-            if response.status_code == 200:
-                chain = response.json()["chain"]
-                length = len(chain)
+                if response.status_code == 200:
+                    chain = response.json()["chain"]
+                    length = len(chain)
 
-                # Check if the length is longer and the chain is valid
-                if length > max_length and self.valid_chain(chain):
-                    max_length = length
-                    new_chain = chain
+                    # Check if the length is longer and the chain is valid
+                    if length > max_length and self.valid_chain(chain):
+                        max_length = length
+                        new_chain = chain
+            except requests.exceptions.ConnectionError:
+                print(f"Node: {node} not responding")
+                pass
 
         # Replace our chain if we discovered a new, valid chain longer than ours
         if new_chain:
